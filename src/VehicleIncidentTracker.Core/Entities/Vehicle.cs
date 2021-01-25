6f6b9cda08a41ea2d406d86fa316e73d8e199649
eyasuberhe <berhe.eyasu@gmail.com>
2021-01-25 05:23:52 -0500

    Revert "Vehicle Incident Tracker"
    This reverts commit f39b47059b8d6be012bbdd95a4d97a9da907e420.

---
+++ src/VehicleIncidentTracker.Core/Entities/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VehicleIncidentTracker.Core.Entities
+{
+    public class Vehicle : BaseEntity
+    {
+        public string VIN { get; private set; }
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+        public string Year { get; private set; }
+
+        private List<Incident> _incidents;
+
+        public IEnumerable<Incident> Incidents => _incidents.AsReadOnly();
+
+        protected Vehicle()
+        {
+
+            _incidents = new List<Incident>();
+        }
+
+        public Vehicle(string vin, string make, string model, string year)
+        {
+            VIN = vin;
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+}
---
+++ src/VehicleIncidentTracker.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using VehicleIncidentTracker.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VehicleIncidentTracker.Core.Entities;
+using VehicleIncidentTracker.Core;
+using System.Reflection;
+using JetBrains.Annotations;
+using VehicleIncidentTracker.Infrastructure.Data.Config;
+
+namespace VehicleIncidentTracker.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new VehicleEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new IncidentEntityConfiguration());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+    }
+}
